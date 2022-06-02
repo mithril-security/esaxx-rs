d5322308ee8356bca41f0fe2b638afe54e151c70
Matthias Brunel <matthias.brunel@mithrilsecurity.io>
2022-06-02 12:23:35 +0200

    add pure-rust feature

--- build.rs
+++ build.rs
@@ -1,9 +1,11 @@
 fn main() {
-    cc::Build::new()
-        .cpp(true)
-        .flag("-std=c++11")
-        .static_crt(true)
-        .file("src/esaxx.cpp")
-        .include("src")
-        .compile("esaxx");
+    if !cfg!(feature = "pure-rust") {
+        cc::Build::new()
+            .cpp(true)
+            .flag("-std=c++11")
+            .static_crt(true)
+            .file("src/esaxx.cpp")
+            .include("src")
+            .compile("esaxx");
+    }
 }
